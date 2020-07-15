dcaa170e3e76335d926d887a005e37cbd2c16748
Meunier-Emmanuel <emmanuel.meunier.dev@gmail.com>
2020-07-15 14:14:07 +0200

    pwa

---
+++ assets/js/serviceWorker.js
@@ -0,0 +1,10 @@
+const takeOFf = "dev-takeOff-site-v1"
+
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
